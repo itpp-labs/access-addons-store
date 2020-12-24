e59ec2449f40125b1ec56226f00ff51be26e6f1c
Denis Mudarisov <denis.mudarisov@gmail.com>
2020-12-24 16:37:37 +0500

    :arrow_up::one::four: web_debranding
    made by @it-projects-llc

--- web_debranding/static/src/js/bot.js
+++ web_debranding/static/src/js/bot.js
@@ -1,44 +1,41 @@
 /*  Copyright 2016-2017 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
+    Copyright 2020 Eugene Molotov <https://it-projects.info/team/em230418>
     License MIT (https://opensource.org/licenses/MIT). */
 odoo.define("web_debranding.bot", function (require) {
     "use strict";
 
     require("web_debranding.dialog");
-    var Message = require("mail.model.Message");
-    var session = require("web.session");
-    var MailBotService = require("mail_bot.MailBotService");
-    var core = require("web.core");
-    var _t = core._t;
+    const components = {
+        Message: require('mail/static/src/components/message/message.js'),
+        ThreadPreview: require('mail/static/src/components/thread_preview/thread_preview.js'),
+    };
 
-    Message.include({
-        _getAuthorName: function () {
-            if (this._isOdoobotAuthor()) {
-                return "Bot";
-            }
-            return this._super.apply(this, arguments);
-        },
-        getAvatarSource: function () {
-            if (this._isOdoobotAuthor()) {
+    const session = require("web.session");
+    const { patch } = require('web.utils');
+
+    patch(components.ThreadPreview, 'web_debranding/static/src/js/bot.js', {
+        image() {
+            if (
+                this.thread.correspondent &&
+                this.thread.correspondent === this.env.messaging.partnerRoot
+            ) {
                 return "/web/binary/company_logo?company_id=" + session.company_id;
             }
-            return this._super.apply(this, arguments);
-        },
+            return this._super();
+        }
     });
 
-    MailBotService.include({
-        getPreviews: function (filter) {
-            var previews = this._super.apply(this, arguments);
-            previews.map(function (preview) {
-                if (preview.title === _t("OdooBot has a request")) {
-                    preview.title = _t("Bot has a request");
-                }
-                if (preview.imageSRC === "/mail/static/src/img/odoobot.png") {
-                    preview.imageSRC =
-                        "/web/binary/company_logo?company_id=" + session.company_id;
-                }
-                return preview;
-            });
-            return previews;
-        },
+    patch(components.Message, 'web_debranding/static/src/js/message.js', {
+        avatar_debranded() {
+            if (
+                this.message.author &&
+                this.message.author === this.env.messaging.partnerRoot
+            ) {
+                return "/web/binary/company_logo?company_id=" + session.company_id;
+            }
+
+            return this.avatar;
+        }
     });
+
 });
