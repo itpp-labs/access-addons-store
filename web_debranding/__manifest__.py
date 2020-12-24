26548d92cba189d6a6f5988b3474815c7333bfde
Mitchell Admin <itpp-bot@users.noreply.github.com>
2020-12-24 10:34:45 +0000

    :tada::one::three: web_debranding previous commits history: https://github.com/itpp-labs/misc-addons-store/commits/13.0/ir_rule_protected
    > Made via .github/workflows/DINAR-PORT.yml

---
+++ web_debranding/__manifest__.py
@@ -0,0 +1,35 @@
+# Copyright 2015-2020 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
+# Copyright 2017 Ilmir Karamov <https://it-projects.info/team/ilmir-k>
+# Copyright 2018-2019 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
+# Copyright 2018 Ildar Nasyrov <https://it-projects.info/team/iledarn>
+# Copyright 2018 WohthaN <https://github.com/WohthaN>
+# Copyright 2019 Eugene Molotov <https://github.com/em230418>
+# Copyright 2020 Denis Mudarisov <https://github.com/trojikman>
+# License MIT (https://opensource.org/licenses/MIT).
+# License OPL-1 (https://www.odoo.com/documentation/user/13.0/legal/licenses/licenses.html#odoo-apps) for derivative work.
+{
+    "name": "Backend debranding",
+    "version": "13.0.1.0.31",
+    "author": "IT-Projects LLC, Ivan Yelizariev",
+    "license": "OPL-1",
+    "category": "Debranding",
+    "images": ["images/web_debranding.png"],
+    "website": "https://odoo-debranding.com",
+    "support": "apps@itpp.dev",
+    "price": 250.00,
+    "currency": "EUR",
+    "depends": ["base_setup", "web", "mail", "access_settings_menu", "mail_bot"],
+    "data": [
+        "data.xml",
+        "views.xml",
+        "js.xml",
+        "pre_install.xml",
+        "views/res_config.xml",
+    ],
+    "qweb": ["static/src/xml/web.xml"],
+    "post_load": "post_load",
+    "auto_install": False,
+    "uninstall_hook": "uninstall_hook",
+    "installable": True,
+    "saas_demo_title": "Backend debranding demo",
+}
