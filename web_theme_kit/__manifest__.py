c2af3214dfab58acb0b7173e2c5120a412ca1980
Ivan Yelizariev // IEL <yelizariev@itpp.dev>
2021-01-04 17:18:13 +0100

    :arrow_up::one::three: delete web_widget_color dep
    it's built-in now

--- web_theme_kit/__manifest__.py
+++ web_theme_kit/__manifest__.py
@@ -10,10 +10,10 @@
     "application": False,
     "author": "IT-Projects LLC, Ivan Yelizariev",
     "website": "https://it-projects.info",
-    "license": "LGPL-3",
+    "license": "Other OSI approved licence",  # MIT
     "price": 50.00,
     "currency": "EUR",
-    "depends": ["web_debranding", "web_login_background", "web_widget_color"],
+    "depends": ["web_debranding", "web_login_background"],
     "external_dependencies": {"python": [], "bin": []},
     "data": [
         "security/ir.model.access.csv",
