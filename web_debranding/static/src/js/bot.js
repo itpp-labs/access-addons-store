4f104e2395b5d2cfac7d75a66d7f7219e7e4f148
Denis Mudarisov <denis.mudarisov@gmail.com>
2021-01-05 13:40:30 +0500

    :rainbow: pre-commit

--- web_debranding/static/src/js/bot.js
+++ web_debranding/static/src/js/bot.js
@@ -1,20 +1,20 @@
 /*  Copyright 2016-2017 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
     Copyright 2020 Eugene Molotov <https://it-projects.info/team/em230418>
-    License MIT (https://opensource.org/licenses/MIT). 
+    License MIT (https://opensource.org/licenses/MIT).
     License OPL-1 (https://www.odoo.com/documentation/user/14.0/legal/licenses/licenses.html#odoo-apps) for derivative work. */
 odoo.define("web_debranding.bot", function (require) {
     "use strict";
 
     require("web_debranding.dialog");
     const components = {
-        Message: require('mail/static/src/components/message/message.js'),
-        ThreadPreview: require('mail/static/src/components/thread_preview/thread_preview.js'),
+        Message: require("mail/static/src/components/message/message.js"),
+        ThreadPreview: require("mail/static/src/components/thread_preview/thread_preview.js"),
     };
 
     const session = require("web.session");
-    const { patch } = require('web.utils');
+    const {patch} = require("web.utils");
 
-    patch(components.ThreadPreview, 'web_debranding/static/src/js/bot.js', {
+    patch(components.ThreadPreview, "web_debranding/static/src/js/bot.js", {
         image() {
             if (
                 this.thread.correspondent &&
@@ -23,10 +23,10 @@ odoo.define("web_debranding.bot", function (require) {
                 return "/web/binary/company_logo?company_id=" + session.company_id;
             }
             return this._super();
-        }
+        },
     });
 
-    patch(components.Message, 'web_debranding/static/src/js/message.js', {
+    patch(components.Message, "web_debranding/static/src/js/message.js", {
         avatar_debranded() {
             if (
                 this.message.author &&
@@ -36,7 +36,6 @@ odoo.define("web_debranding.bot", function (require) {
             }
 
             return this.avatar;
-        }
+        },
     });
-
 });
