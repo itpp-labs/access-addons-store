cdcaac8e623c63b0c2074f1caac8cdcab2486376
Ivan Yelizariev <iel@odoo.com>
2022-05-25 17:51:50 +0200

    :arrow_up: port web_debranding to v15

--- web_debranding/static/src/js/base.js
+++ web_debranding/static/src/js/base.js
@@ -1,33 +1,31 @@
-/*  Copyright 2015-2018 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
+/** @odoo-module **/
+/*  Copyright 2015-2018,2022 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
     Copyright 2017 ArtyomLosev <https://github.com/ArtyomLosev>
     License MIT (https://opensource.org/licenses/MIT). */
-odoo.define("web_debranding.base", function (require) {
-    "use strict";
-    var WebClient = require("web.WebClient");
+import { WebClient } from "@web/webclient/webclient";
 
-    WebClient.include({
-        init: function (parent) {
-            this._super.apply(this, arguments);
-            var self = this;
-            this.set("title_part", {zopenerp: ""});
-            odoo.debranding_new_name = "";
-            odoo.debranding_new_website = "";
-            odoo.debranding_new_title = "";
+export class WebClientDebranded extends WebClient {
+    setup() {
+        const self = this;
+        super.setup(...arguments);
+        this.title.setParts({ zopenerp: "" });
+        odoo.debranding_new_name = "";
+        odoo.debranding_new_website = "";
+        odoo.debranding_new_title = "";
 
-            self._rpc(
-                {
-                    model: "ir.config_parameter",
-                    method: "get_debranding_parameters",
-                },
-                {
-                    shadow: true,
-                }
-            ).then(function (result) {
-                odoo.debranding_new_name = result["web_debranding.new_name"];
-                odoo.debranding_new_website = result["web_debranding.new_website"];
-                odoo.debranding_new_title = result["web_debranding.new_title"];
-                self.set("title_part", {zopenerp: odoo.debranding_new_title});
-            });
-        },
-    });
-});
+        this.rpc(
+            {
+                model: "ir.config_parameter",
+                method: "get_debranding_parameters",
+            },
+            {
+                shadow: true,
+            }
+        ).then(function (result) {
+            odoo.debranding_new_name = result["web_debranding.new_name"];
+            odoo.debranding_new_website = result["web_debranding.new_website"];
+            odoo.debranding_new_title = result["web_debranding.new_title"];
+            this.title.setParts({zopenerp: odoo.debranding_new_title});
+        });
+    }
+}
