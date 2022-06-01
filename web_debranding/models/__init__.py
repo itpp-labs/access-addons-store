d501774bab98d6540e0842f41cf5298c3b7fc9e4
Ivan Yelizariev <iel@odoo.com>
2022-06-01 15:38:16 +0200

    :arrow_up: web_debranding: merge IT-Projects' port to v15

--- web_debranding/models/__init__.py
+++ web_debranding/models/__init__.py
@@ -11,5 +11,5 @@ from . import mail_channel
 from . import res_users
 from . import ir_model
 from . import res_company
-from . import res_config_settings
 from . import ir_module_module
+from . import res_config_settings
--- web_debranding/static/src/js/dialog.js
+++ web_debranding/static/src/js/dialog.js
@@ -1,49 +1,42 @@
+/** @odoo-module **/
 /* Copyright 2015-2018,2021 Ivan Yelizariev <https://twitter.com/yelizariev>
    Copyright 2015 igallyamov <https://github.com/igallyamov>
    Copyright 2017 Gabbasov Dinar <https://it-projects.info/team/GabbasovDinar>
-   License MIT (https://opensource.org/licenses/MIT).*/
-odoo.define("web_debranding.dialog", function (require) {
-    "use strict";
-    require("web_debranding.base");
-    var CrashManager = require("web.CrashManager");
+   Copyright 2022 IT-Projects <https://it-projects.info/>
+   License OPL-1 (https://www.odoo.com/documentation/user/14.0/legal/licenses/licenses.html#odoo-apps). */
 
-    function crash_init(parent, options, error) {
-        var debranding_new_name = odoo.debranding_new_name;
-        error.message = error.message.replace(/Odoo/gi, debranding_new_name);
-        this._super.apply(this, [parent, options, error]);
-    }
-    CrashManager.ErrorDialog.include({
-        init: crash_init,
-    });
-    CrashManager.WarningDialog.include({
-        init: crash_init,
-    });
+import "@web_debranding/js/base";
+import { Dialog } from "@web/core/dialog/dialog";
+import { patch } from "web.utils";
 
-    var Dialog = require("web.Dialog");
-    Dialog.include({
-        init: function (parent, options) {
-            var debranding_new_name = odoo.debranding_new_name;
-            var debranding_new_website = odoo.debranding_new_website;
-            options = options || {};
-            if (options.title && options.title.replace) {
-                var title = options.title.replace(/Odoo/gi, debranding_new_name);
-                options.title = title;
-            } else {
-                options.title = debranding_new_name;
+const component = { Dialog };
+
+patch(component.Dialog.prototype, "web_debranding/static/src/js/dialog.js", {
+    setup() {
+        const debranding_new_name = odoo.debranding_new_name;
+        // // const debranding_new_website = odoo.debranding_new_website;
+
+        // // options = this.constructor || {};
+
+        if (this.constructor.title && this.constructor.title.replace) {
+            var title = this.constructor.title.replace(/Odoo/gi, debranding_new_name);
+            this.constructor.title = title;
+        } else {
+            this.constructor.title = debranding_new_name;
+        }
+        // Разобраться во что превратился $content
+        /* if (options.$content) {
+            if (!(options.$content instanceof $)) {
+                options.$content = $(options.$content);
             }
-            if (options.$content) {
-                if (!(options.$content instanceof $)) {
-                    options.$content = $(options.$content);
-                }
-                var content_html = options.$content.html();
-                content_html = content_html.replace(
-                    /Odoo.com/gi,
-                    debranding_new_website
-                );
-                content_html = content_html.replace(/Odoo/gi, debranding_new_name);
-                options.$content.html(content_html);
-            }
-            this._super(parent, options);
-        },
-    });
+            var content_html = options.$content.html();
+            content_html = content_html.replace(
+                /Odoo.com/gi,
+                debranding_new_website
+            );
+            content_html = content_html.replace(/Odoo/gi, debranding_new_name);
+            options.$content.html(content_html);
+        }*/
+        this._super();
+    },
 });
