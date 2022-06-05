c20ac1ec64a6cabfd6817a4d93a6e6ead101026b
Ivan Yelizariev <iel@odoo.com>
2022-06-05 12:25:57 +0200

    :x: delete unused field
    it was used in v12 to hide debug links for non admin

--- web_debranding/models/res_users.py
+++ web_debranding/models/res_users.py
@@ -8,8 +8,3 @@ class ResUsers(models.Model):
     _inherit = "res.users"
 
     odoobot_state = fields.Selection(string="Bot Status")
-
-    def is_admin(self):
-        # By default Python functions starting with _ are considered private methods.
-        # Private methods (such as _is_admin) cannot be called remotely
-        return self._is_admin()
