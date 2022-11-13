e5da9ef4c2cfa490f22d4eff4096715762277a2f
Mitchell Admin <itpp-bot@users.noreply.github.com>
2022-11-13 13:48:30 +0000

    :arrow_up::one::six: OCA/odoo-module-migrator
    close #61
    
    > Made via .github/workflows/DINAR-PORT.yml

--- website_debranding/__manifest__.py
+++ website_debranding/__manifest__.py
@@ -3,7 +3,7 @@
 # License OPL-1 (https://www.odoo.com/documentation/user/13.0/legal/licenses/licenses.html#odoo-apps) for derivative work.
 {
     "name": "Website debranding",
-    "version": "15.0.1.0.1",
+    "version": "16.0.1.0.1",
     "author": "IT-Projects LLC, Ivan Yelizariev",
     "license": "OPL-1",
     "category": "Debranding",
@@ -14,5 +14,5 @@
     "data": ["views.xml"],
     "demo": ["demo.xml"],
     "auto_install": True,
-    "installable": False,
+    "installable": True,
 }
